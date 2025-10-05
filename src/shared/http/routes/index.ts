c05409a0c0a3b8cd3351d59a932815c3a7099237
Vinicius Sartini <sartini.vinicius@outlook.com>
2025-10-05 12:39:13 -0300

    chore: Criação do CRUD do modelo Alerta

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -5,6 +5,7 @@ import { distributorsRouter } from "modules/distributors/distributors.routes.js"
 import { propertiesRouter } from "modules/properties/properties.routes.js"
 import { iotInsertionRouter, iotRouter } from "modules/iot/iot.routes.js"
 import { consumptionRouter } from "modules/consumption/consumption.routes.js"
+import { alertsRouter } from "modules/alerts/alerts.routes.js"
 
 const mainRouter = Router()
 
@@ -15,5 +16,6 @@ mainRouter.use('/properties', propertiesRouter)
 mainRouter.use('/iot-devices', iotRouter)
 mainRouter.use('/consumption', consumptionRouter)
 mainRouter.use('/iot', iotInsertionRouter)
+mainRouter.use('/alerts', alertsRouter)
 
 export { mainRouter }
