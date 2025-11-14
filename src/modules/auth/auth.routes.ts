298c76606a566d643e1e31d25f37f2c4891046c0
Vinicius Sartini <sartini.vinicius@outlook.com>
2025-11-13 21:28:35 -0300

    enhance: Validação do módulo auth

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import { AuthController } from "./auth.controller.js"
 import { isAuthenticated } from "shared/middlewares/isAuthenticated.js"
+import { validateRequest } from "shared/middlewares/validateRequest.js"
+import { loginSchema, registerSchema } from "./auth.schemas.js"
 
 const authRouter = Router()
 const authController = new AuthController()
 
-authRouter.post("/register", authController.register)
-authRouter.post("/login", authController.login)
+authRouter.post("/register", validateRequest(registerSchema), authController.register)
+authRouter.post("/login", validateRequest(loginSchema) , authController.login)
 authRouter.post("/logout", isAuthenticated, authController.logout)
 
 export { authRouter }
--- src/modules/auth/auth.types.ts
+++ src/modules/auth/auth.types.ts
@@ -1,33 +1,5 @@
-type UserBaseDTO = {
-    email: string
-    password: string
-    name: string
-}
+import { z } from "zod";
+import type { loginSchema, registerSchema } from "./auth.schemas.js";
 
-type PhysicalPersonDTO = {
-    userType: 'PHYSICAL'
-    birthDate: string // Converte para DATE no service
-    zipCode: string
-    address: string
-    city: string
-    state: string
-    country: string
-}
-
-type CorporationDTO = {
-    userType: 'CORPORATION'
-    fantasyName: string
-    cnpj: string
-    zipCode: string
-    address: string
-    city: string
-    state: string
-    country: string
-}
-
-export type RegisterUserDTO = UserBaseDTO & (PhysicalPersonDTO | CorporationDTO)
-
-export type LoginUserDTO = {
-    email: string
-    password: string
-}
+export type LoginUserDTO = z.infer<typeof loginSchema>
+export type RegisterUserDTO = z.infer<typeof registerSchema>
